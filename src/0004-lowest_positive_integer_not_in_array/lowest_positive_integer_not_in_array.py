8d8ff3352428ba3e9f03e5d0d8c466269aa472d4
Simon Lammer <lammer.simon@gmail.com>
2019-04-30 08:53:59 +0200

    dcp4: verify solutions

--- src/0004-lowest_positive_integer_not_in_array/lowest_positive_integer_not_in_array.py
+++ src/0004-lowest_positive_integer_not_in_array/lowest_positive_integer_not_in_array.py
@@ -16,7 +16,7 @@ def main(arr):
   for i, num in enumerate(arr):
     num = abs(num)
     if 0 < num and num <= len(arr):
-      if num > i: # and num + 1 != i
+      if num > i:
         if arr[num - 1] > 0:
           arr[num - 1] = -arr[num - 1]
       else:
@@ -28,7 +28,6 @@ def main(arr):
 
 
 def linear_space(arr):
-  # Fails with [8, 1, 10, 6, 2, 4, 0, 0, 12, 1] (returns 1 instead of 3)
   found_arr = [False] * len(arr)
   for num in arr:
     if 0 < num and num <= len(arr):
@@ -39,8 +38,6 @@ def linear_space(arr):
   return len(arr) + 1
 
 def simple(arr):
-  # Fails with [3, 1, 2, 12, -8, 10, 12, 11, 5, 7] (returns 1 instead of 4)
-  # Fails with [4, 11, 2, 1, 3, 1, 0, 7, 5, -1] (returns 2 instead of 6)
   lowest_missing_int = 1
   found_new = True
   while found_new:
@@ -57,16 +54,16 @@ if __name__ == '__main__':
   assert main([1, 2, 0]) == 3
 
   import random
-  arr_size = 10
+  arr_size = 50
   arr = [0] * arr_size
-  for i in range(1000):
+  for i in range(10000):
     for j in range(arr_size):
       arr[j] = random.randint(0, (int(arr_size * 5 / 4)))
       if random.random() > 0.9:
         arr[j] = -arr[j]
     old_arr = arr.copy()
-    actual = main(arr)
     expected = simple(arr)
+    actual = main(arr)
     if actual != expected:
       print(f"{old_arr} -> {actual} (instead of {expected})")
       import pdb; pdb.set_trace()
